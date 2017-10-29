646a96bb01b7e643fba6eb1ac51a755138f87132
Drew Hagen <drew.a.hagen@gmail.com>
2017-10-29 14:28:15 -0500

    set up sass, javascript changes iframe src

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,3 +4,31 @@ var width = document.body.clientWidth;
 
 hi.setAttribute("height", height);
 hi.setAttribute("width", width);
+
+function frameSwap(page) {
+  if (page=="home")
+  {
+
+  }
+  else if (page=="about")
+  {
+
+  }
+  else if (page=="contact")
+  {
+
+  }
+  else if (page=="education")
+  {
+
+  }
+  else if (page=="skills")
+  {
+
+  }
+  else if (page=="work")
+  {
+    
+  }
+
+}
